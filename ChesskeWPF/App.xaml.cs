e37eb5cb8bb6e0403060f828bcf576ba6e1b0175
Zsolt Szigetközi <je0rhr@INF.ELTE.HU>
2022-12-12 23:13:55 +0100

    .

--- ChesskeWPF/App.xaml.cs
+++ ChesskeWPF/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace ChesskeWPF
+namespace Chesske
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- ChesskeWPF/View/MainWindow.xaml.cs
+++ ChesskeWPF/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ChesskeWPF
+namespace Chesske.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
